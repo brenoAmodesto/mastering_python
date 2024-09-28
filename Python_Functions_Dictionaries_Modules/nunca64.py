fe3676d52019f10395519fbe859fe0b561173d8e
brenoAmodesto <brenoamodesto58@gmail.com>
2024-09-28 01:13:55 -0300

    chewing-gum

---
+++ Python_Functions_Dictionaries_Modules/nunca64.py
@@ -0,0 +1,7 @@
+from sys import path
+
+from nunca64_package.modulo import soma_do_modulo
+from nunca64_package.modulo import *
+
+#print(*path, sep="\n")
+print(soma_do_modulo(1,1))
---
+++ Python_Functions_Dictionaries_Modules/nunca64_package/modulo.py
@@ -0,0 +1,10 @@
+__all__ = [
+    
+    'variavel',
+]
+
+
+variavel = "Alguma coisa"
+
+def soma_do_modulo(x, y):
+    return x + y

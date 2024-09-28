74aa515e029659d0b73c0f1db74f21ad6339d420
brenoAmodesto <brenoamodesto58@gmail.com>
2024-09-28 02:23:26 -0300

    packages_e___init__.py

--- Python_Functions_Dictionaries_Modules/nunca64.py
+++ Python_Functions_Dictionaries_Modules/nunca64.py
@@ -1,9 +1,13 @@
-from sys import path
+#from sys import path
 
-from nunca64_package.modulo import fala_oi, soma_do_modulo
+#from nunca64_package.modulo import fala_oi, soma_do_modulo
 
 #print(*path, sep="\n")
 
-print(soma_do_modulo(1,1))
+#print(soma_do_modulo(1,1))
 
-fala_oi()
+#fala_oi()
+
+import nunca64_package
+
+print(nunca64_package.dobra(2))

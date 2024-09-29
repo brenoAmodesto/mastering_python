1a66f4d38bc6f4bd6b439cfe2adbd938ba4758b2
brenoAmodesto <brenoamodesto58@gmail.com>
2024-09-29 05:11:32 -0300

    adiando_funcoes

---
+++ Python_Functions_Dictionaries_Modules/nunca66.py
@@ -0,0 +1,17 @@
+# Exercício - Adiando execução de funções
+
+def soma (x, y):
+    return x + y
+
+
+def multiplica(x, y):
+    return x * y
+
+
+def executa(funcao, *args):
+    return x * y
+
+
+soma_com_cinco = executa(soma, 5)
+
+multilpica_por_dez = executa(multiplica, 10)

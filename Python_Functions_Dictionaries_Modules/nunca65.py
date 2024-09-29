cd706173d8a521025ff0a482e07ac6f58153008b
brenoAmodesto <brenoamodesto58@gmail.com>
2024-09-29 02:53:22 -0300

    deep_copy

--- Python_Functions_Dictionaries_Modules/nunca65.py
+++ Python_Functions_Dictionaries_Modules/nunca65.py
@@ -2,17 +2,76 @@
 # Aumente os preços dos produtos a seguir em 10%
 # Gere novos_produtos por deep copy
 
-produtos = [
-    {'nome': 'Produto 5', 'preco': 10.00},
-    {'nome': 'Produto 1', 'preco': 22.32},
-    {'nome': 'Produto 3', 'preco': 10.11},
-    {'nome': 'Produto 2', 'preco': 105.87},
-    {'nome': 'Produto 4', 'preco': 69.90},
- 
-]
-
 # Ordene os produtos por nome decrescente (do maior para menor)
 # Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
 
 # Ordene os produtos por preco crescente (do menor para maior)
 # Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
+# copy, sorted, produtos.sort
+
+import copy
+from dados import produtos
+
+# Exemplo usando list compreheension
+
+novos_produtos = [
+    {**p, 'preco': round(p['preco'] * 1.1, 2)}
+    for p in copy.deepcopy(produtos)
+]
+
+
+# print(*produtos, sep ='\n')
+# print()
+# print(*novos_produtos, sep='\n')
+
+# Ordene os produtos por nome decrescente (do maior para menor)
+# Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
+
+produtos_ordenados_por_nome = sorted(
+    copy.deepcopy(produtos),
+    key=lambda p: p['nome'],
+    reverse=True
+)
+
+
+# print(*produtos, sep ='\n')
+# print()
+# print(*produtos_ordenados_por_nome, sep='\n')
+
+
+# Ordene os produtos por preco crescente (do menor para maior)
+# Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
+
+produtos_ordenados_por_preco = sorted(
+    copy.deepcopy(produtos),
+    key=lambda p: p['preco'],
+)
+
+print(*produtos, sep ='\n')
+print()
+print(*produtos_ordenados_por_preco, sep='\n')
+
+
+
+
+# Do jeito que eu fiz
+
+# deep_copy_produtos = copy.deepcopy(produtos)
+
+# fator = 1.10
+
+# for dicionarios in deep_copy_produtos:
+#        preco = (dicionarios['preco'])
+#        dicionarios['preco'] = round(dicionarios["preco"] * fator, 2)
+
+# produtos_ordenados_nome = sorted(deep_copy_produtos, key=lambda x: x["nome"],  reverse=True)
+
+# print(produtos_ordenados_nome)
+
+# deep_copy_preco = copy.deepcopy(produtos_ordenados_nome)
+
+# produtos_ordenados_preco = sorted(deep_copy_preco, key=lambda x: x["preco"])
+
+# print(produtos_ordenados_preco)
+
+

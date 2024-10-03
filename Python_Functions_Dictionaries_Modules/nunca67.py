4e6dc33eb9cdf09e03a6460ad1149c5d30513758
brenoAmodesto <brenoamodesto58@gmail.com>
2024-10-03 15:53:00 -0300

    func

--- Python_Functions_Dictionaries_Modules/nunca67.py
+++ Python_Functions_Dictionaries_Modules/nunca67.py
@@ -1,9 +1,29 @@
-# Variáveis livres + nonlocal
-def fora():
-    a = x
+# # Variáveis livres + nonlocal (locals, globals)
+# def fora(x):
+#     a = x
 
-    def dentro():
-        return a
-    return dentro
+#     def dentro():
+#         return a
+#     return dentro
 
-dentro = fora(10)
+
+# dentro1 = fora(10)
+# dentro2 = fora(20)
+
+# print(dentro1())
+# print(dentro2())
+
+def concatenar(string_inicial):
+    valor_final = string_inicial
+
+    def interna(valor_a_concatenar):
+        nonlocal valor_final
+        
+        valor_final += valor_a_concatenar
+        return valor_final
+    return interna
+
+c = concatenar('a')
+print(c('b'))
+print(c('c'))
+print(c('d'))
---
+++ Python_Functions_Dictionaries_Modules/nunca68.py
@@ -0,0 +1,8 @@
+# Funções decoradoras e decoradores
+# Decorar = Adicionar / Remover / Restringir / Alterar
+# Funções decoradoras são funções que decoram outras funções
+# Decoradores são usados para fazer o Python
+# usar as funções decoradoras em outras funções.
+
+def inverte_string(string):
+    return string[::-1]
